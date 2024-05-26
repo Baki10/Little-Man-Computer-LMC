5d51eb75e4f1b4d8137a7623f23acea41a264a48
Baki10 <157256204+Baki10@users.noreply.github.com>
2024-05-26 14:38:16 +0200

    Added:  simulated registers         simulated memory         instruction reader

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+#![allow(non_snake_case)]
+
+fn executeInstruction(pc: &mut u8, acc: &mut u16, flag: &mut bool, mem: &mut Vec<u16>) {
+
+    let index: u8 = *pc;
+
+    let instruction: u8 = (mem[usize::from(index)]/100) as u8;
+    let address: u8 = (mem[usize::from(index)]%100) as u8;
+
+    
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut memory: Vec<u16> = vec![0; 100];
+    let mut accumulator: u16 = 0;
+    let mut programCounter: u8 = 0;
+    let mut flag: bool = false;
+
+    println!("Size : {}", memory[99]);
 }
